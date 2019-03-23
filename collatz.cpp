575aa41c5426c251a219f764f53af93adc65885d
kevin-fallas-b <kevin.fallas.b@gmail.com>
2019-03-22 20:18:30 -0600

    finalizacion collatz

--- collatz.cpp
+++ collatz.cpp
@@ -3,54 +3,42 @@
 #include <unistd.h>       //se incluye para poder usar fork
 #include <signal.h>       //se incluye para usar kill
 #include <sys/mman.h>     //se incluye para poder usar mmap
-#include <vector>
 
 using namespace std;
 
-
 int main(int argc, char *argv[]){
   //validacion de argumentos recibidos
-  cout << "inicio programa";
   if( argc != 2 || !(isdigit(*argv[1])) ){
-    cout << "Argumento invalido.\nPor favor ejecute el programa con un numero de argumento.\nejm: ./collatz 8\n" << endl;
+    cout << "Argumento invalido.\nPor favor ejecute el programa con un numero de argumento.\nejm: ./Collatz 3\n" << endl;
     return 0;
   }
-  int numero = atoi(argv[1]);                //guardo el numero en una variable para facilidad de lectura
-  cout << "se hizo atoi";
-  //int fd[2]; //0 se usa para leer, 1 para escribir
-  //if(pipe(fd) == -1){
-    //cout << "Error en el pipe";
-    //return 0;
-  //}
-  //pipe(fd);
+  int numero = atoi(argv[1]);              //guardo el numero para facilidad de lectura
+  // la siguiente linea es donde sucede la magia, creo dinamicamente el array donde voy a guardar la secuencia
+  // ademas de eso, uso mmap para mapear en memoria ese array, lo que me permite trabajar como si fuera una variable global entre todos los hilos
+  // ver la pagina man de mmap para detalles de parametros.
+  int *secuencia = (int*)mmap(NULL, 100,PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED,-1, 0);
   pid_t hijo = fork();                       //creo el proceso hijo
+
   if(hijo == 0){
-    cout << "se entro a hijo";
-    //vector<int> secuencia;
-    cout << numero;
-    //secuencia.push_back(numero);
+    int cont=0;
     while (numero > 1){
-      cout << numero;
+      secuencia[cont]=numero;
       if(numero % 2 == 0){
         numero = numero/2;
       }else{
         numero = (numero *3)+1;
       }
-      cout << "se va a intentar push back de: " << numero;
-      //secuencia.push_back(numero);
+      cont++;
     }
-    cout << "termino de encontrar la secuencia";
-    //aqui escribo el vector por medio de pipe
-    //write(fd[1],(void*)&secuencia,secuencia.size()+1);
-    //close(fd[1]);
-    kill(getpid(),SIGTERM);
+    secuencia[cont]=numero;
+    kill(getpid(),SIGTERM);                  //kill al hilo hijo para que no me duplique la impresion en pantalla
   }
-  //recibe el pipe, imprime
-  //vector<int> resultado;
-  //read(fd[0],&resultado,1);
-  //close(fd[0]);
-  //for(int i=0;i<resultado.size();i++){
-    //cout << resultado.at(i);
-  //}
+  cout << "La secuencia es la siguiente:" << endl;
+  for(int i=0;i>=0;i++){
+    cout << secuencia[i] << endl;
+    if(secuencia[i]==1){
+      break;
+    }
+  }
   return 0;
 }
